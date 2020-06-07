cb75caa8c4c64947e1f5c3689928d1e7d9243d33
yarencelik <yarencelik373@gmail.com>
2020-06-08 02:32:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+// http://nodejs.org/docs/v0.6.5/api/fs.html#fs.writeFile
+var fs = require('fs');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "blogum"
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM post ', function(err, results, fields) {
+    if (err) throw err;
+
+    fs.writeFile('table.json', JSON.stringify(results), function(err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    connection.end();
+});
